de2bf877b0a8405d93f422c7169bf2f0e9fcd192
Muneer Ahmed Syed <muneer@ishtech.fi>
2020-09-05 07:19:53 +0300

    Initial commit

---
+++ src/main/java/fi/ishtech/utils/HeTuUtil.java
@@ -0,0 +1,144 @@
+/**
+ * 
+ */
+package fi.ishtech.utils;
+
+import java.time.LocalDate;
+import java.util.regex.Pattern;
+
+/**
+ * Util class for Finnish Social Security ID / Henkilo tunnus
+ *
+ * @author Muneer Ahmed Syed
+ */
+public class HeTuUtil {
+
+	private static final String PLUS = "+";
+	private static final String HYPHEN = "-";
+	private static final String A = "A";
+
+	private static final String EIGHTEEN = "18";
+	private static final String NINETEEN = "19";
+	private static final String TWENTY = "20";
+
+	private static final String CHECKSUM_STR = "0123456789ABCDEFHJKLMNPRSTUVWXY";
+	private static final int CHECKSUM_NUM = 31;
+
+	public static final String SIMPLE_REGEX = "[0-9]{6}[A\\-]{1}[0-9]{3}[A-Z0-9]{1}";
+	public static final String COMP_REGEX = "[0-9]{6}[A\\-+]{1}[0-9]{3}[A-Z0-9]{1}";
+
+	/**
+	 * Validates using regular expression [0-9]{6}[A\\-]{1}[0-9]{3}[A-Z0-9]{1}
+	 *
+	 * @param hetu
+	 * @return boolean - true if valid, false if invalid
+	 */
+	public static boolean isValidByRegex(String hetu) {
+		if (hetu == null) {
+			return false;
+		}
+
+		return Pattern.matches(SIMPLE_REGEX, hetu);
+	}
+
+	/**
+	 * Validates using regular expression [0-9]{6}[A\\-]{1}[0-9]{3}[A-Z0-9]{1} and also checks if the date is valid
+	 *
+	 * @param hetu
+	 * @return boolean - true if valid, false if invalid
+	 */
+	public static boolean isValid(String hetu) {
+		if (isValidByRegex(hetu)) {
+			try {
+				toDateOfBirth(hetu);
+				return true;
+			} catch (Exception e) {
+				return false;
+			}
+		}
+
+		return false;
+	}
+
+	/**
+	 * Finds Date of birth of person based on Henkilo-tunnus (Finnish Social Security Id)
+	 *
+	 * @param hetu
+	 * @return {@link LocalDate} date-of-birth
+	 */
+	public static LocalDate toDateOfBirth(String hetu) {
+		if (hetu == null) {
+			throw new NullPointerException("Input cannot be null");
+		}
+
+		if (hetu.trim().isEmpty()) {
+			throw new RuntimeException("Input cannot be empty");
+		}
+
+		// TODO: validate format
+
+		String d = hetu.substring(0, 2);
+		String m = hetu.substring(2, 4);
+		String y = hetu.substring(4, 6);
+
+		String yearChecker = hetu.substring(6, 7);
+
+		if (A.equalsIgnoreCase(yearChecker)) {
+			y = TWENTY + y;
+		} else if (HYPHEN.equals(yearChecker)) {
+			y = NINETEEN + y;
+		} else if (PLUS.equals(yearChecker)) {
+			y = EIGHTEEN + y;
+		} else {
+			throw new RuntimeException("Invalid input, only '+' or '-' or 'A' allowed at 7 th character");
+		}
+
+		LocalDate result = LocalDate.of(Integer.parseInt(y), Integer.parseInt(m), Integer.parseInt(d));
+
+		return result;
+	}
+
+	public static boolean isValidChecksum(String hetu) {
+		if (isValidByRegex(hetu)) {
+			String ddmmyy = hetu.substring(0, 6);
+			String zzz = hetu.substring(7, 10);
+
+			char actualQ = hetu.charAt(10);
+			char expectedQ = checksum(ddmmyy, zzz);
+
+			return (actualQ == expectedQ);
+		}
+
+		return false;
+	}
+
+	private static char checksum(String ddmmyy, String zzz) {
+		if (ddmmyy == null) {
+			throw new NullPointerException("Input ddmmyy cannot be null");
+		}
+
+		if (ddmmyy.trim().length() != 6) {
+			throw new RuntimeException("Invalid input ddmmyy" + ddmmyy);
+		}
+
+		Integer.parseInt(zzz);
+
+		if (zzz == null) {
+			throw new NullPointerException("Input zzz cannot be null");
+		}
+
+		if (zzz.trim().length() != 3) {
+			throw new RuntimeException("Invalid input zzz " + zzz);
+		}
+
+		Integer.parseInt(zzz);
+
+		Integer ddmmyyzzz = Integer.parseInt(ddmmyy.concat(zzz));
+		int n = ddmmyyzzz % CHECKSUM_NUM;
+
+		char q = CHECKSUM_STR.charAt(n);
+
+		return q;
+	}
+
+}
---
+++ src/test/java/fi/ishtech/utils/HeTuUtilTest.java
@@ -0,0 +1,191 @@
+package fi.ishtech.utils;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.DateTimeException;
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(OrderAnnotation.class)
+public class HeTuUtilTest {
+
+	@Test
+	@Order(1)
+	public void testIsValidByRegexNull() {
+		String hetu = null;
+		boolean actual = HeTuUtil.isValidByRegex(hetu);
+		assertFalse(actual);
+	}
+
+	@Test
+	@Order(1)
+	public void testIsValidByRegexBlank() {
+		String hetu = "";
+		boolean actual = HeTuUtil.isValidByRegex(hetu);
+		assertFalse(actual);
+	}
+
+	@Test
+	@Order(3)
+	public void testIsValidByRegexALastCharAlpha() {
+		String hetu = "321316A855X";
+		boolean actual = HeTuUtil.isValidByRegex(hetu);
+		assertTrue(actual);
+	}
+
+	@Test
+	@Order(4)
+	public void testIsValidByRegexALastCharNum() {
+		String hetu = "321316A8550";
+		boolean actual = HeTuUtil.isValidByRegex(hetu);
+		assertTrue(actual);
+	}
+
+	@Test
+	@Order(5)
+	public void testIsValidByRegexHyphenLastCharNum() {
+		String hetu = "321316-8550";
+		boolean actual = HeTuUtil.isValidByRegex(hetu);
+		assertTrue(actual);
+	}
+
+	@Test
+	@Order(6)
+	public void testIsValidByRegexHyphenLastCharAlpha() {
+		String hetu = "321316-855T";
+		boolean actual = HeTuUtil.isValidByRegex(hetu);
+		assertTrue(actual);
+	}
+
+	@Test
+	@Order(7)
+	public void testIsValidByRegexHyphenLastCharDifferent() {
+		String hetu = "321316-855+";
+		boolean actual = HeTuUtil.isValidByRegex(hetu);
+		assertFalse(actual);
+	}
+
+	@Test
+	@Order(8)
+	public void testIsValidByRegexCenturyCharDifferent() {
+		String hetu = "321316B855T";
+		boolean actual = HeTuUtil.isValidByRegex(hetu);
+		assertFalse(actual);
+	}
+
+	@Test
+	@Order(9)
+	public void testIsValidByRegexCharsInDate() {
+		String hetu = "A21316-855J";
+		boolean actual = HeTuUtil.isValidByRegex(hetu);
+		assertFalse(actual);
+	}
+
+	@Test
+	@Order(10)
+	public void testIsValidByRegexCharsInMonth() {
+		String hetu = "32C316-855J";
+		boolean actual = HeTuUtil.isValidByRegex(hetu);
+		assertFalse(actual);
+	}
+
+	@Test
+	@Order(11)
+	public void testIsValidByRegexCharsInYear() {
+		String hetu = "3213E6-855J";
+		boolean actual = HeTuUtil.isValidByRegex(hetu);
+		assertFalse(actual);
+	}
+
+	@Test
+	@Order(12)
+	public void testToDateOfBirthNineteen() {
+		String hetu = "010216-8550";
+		LocalDate expected = LocalDate.parse("1916-02-01");
+		LocalDate actual = HeTuUtil.toDateOfBirth(hetu);
+
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	@Order(13)
+	public void testToDateOfBirthTwenty() {
+		String hetu = "290216A8550";
+		LocalDate expected = LocalDate.parse("2016-02-29");
+		LocalDate actual = HeTuUtil.toDateOfBirth(hetu);
+
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	@Order(14)
+	public void testToDateOfBirthInvalidNull() {
+		String hetu = null;
+		assertThrows(NullPointerException.class, () -> HeTuUtil.toDateOfBirth(hetu));
+	}
+
+	@Test
+	@Order(15)
+	public void testToDateOfBirthInvalidBlank() {
+		String hetu = " ";
+		assertThrows(RuntimeException.class, () -> HeTuUtil.toDateOfBirth(hetu));
+	}
+
+	@Test
+	@Order(16)
+	public void testToDateOfBirthInvalidCentury() {
+		String hetu = "271016B8550";
+		assertThrows(RuntimeException.class, () -> HeTuUtil.toDateOfBirth(hetu));
+	}
+
+	@Test
+	@Order(17)
+	public void testToDateOfBirthInvalidNumber() {
+		String hetu = "ABCDEF-GHIJ";
+		assertThrows(NumberFormatException.class, () -> HeTuUtil.toDateOfBirth(hetu));
+	}
+
+	@Test
+	@Order(18)
+	public void testToDateOfBirthInvalidMonth() {
+		String hetu = "311316A8550";
+		assertThrows(DateTimeException.class, () -> HeTuUtil.toDateOfBirth(hetu));
+	}
+
+	@Test
+	@Order(19)
+	public void testToDateOfBirthInvalidDate32() {
+		String hetu = "321216A8550";
+		assertThrows(DateTimeException.class, () -> HeTuUtil.toDateOfBirth(hetu));
+	}
+
+	@Test
+	@Order(20)
+	public void testToDateOfBirthInvalidDateNon31() {
+		String hetu = "311116A8550";
+		assertThrows(DateTimeException.class, () -> HeTuUtil.toDateOfBirth(hetu));
+	}
+
+	@Test
+	@Order(21)
+	public void testToDateOfBirthInvalidDateNonLeap() {
+		String hetu = "290215A855X";
+		assertThrows(DateTimeException.class, () -> HeTuUtil.toDateOfBirth(hetu));
+	}
+
+	@Test
+	@Order(22)
+	public void testIsValidChecksum() {
+		String hetu = "010216-855Y";
+		boolean actual = HeTuUtil.isValidChecksum(hetu);
+		assertTrue(actual);
+	}
+
+}
